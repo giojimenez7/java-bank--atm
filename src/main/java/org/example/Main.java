583ab844d3d23bdc3f3627b23b1f0fd2ea76e8f5
Giovanny Jimenez <giojimarc@gmail.com>
2024-12-19 22:13:10 -0500

    Se agregó funcionalidad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello JavaBank ATM!");
+        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
     }
 }
